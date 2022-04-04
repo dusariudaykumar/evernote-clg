b868a5dd8417511a74d806ae6337a1c1e19757c8
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-04 19:19:16 +0530

    updated: index.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { LandingPage } from "./Pages/LandingPage/LandingPage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
