df79e8860aa6a211f6d13f518137d4b0e29217ed
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-09 19:49:20 +0530

    Update auth-context.js

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -2,9 +2,9 @@ import { createContext, useEffect, useContext, useReducer } from "react";
 import { authReducer } from "../reducers/authReducer";
 
 const initialAuth = {
-  isAuth: false,
-  encodedToken: "",
-  userData: "",
+  isAuth: localStorage.getItem("token") ? true : false,
+  encodedToken: localStorage.getItem("token") || "",
+  userData: JSON.parse(localStorage.getItem("userData")) || "",
 };
 
 const AuthContext = createContext(initialAuth);
@@ -12,15 +12,6 @@ const useAuth = () => useContext(AuthContext);
 const AuthProvider = ({ children }) => {
   const [authState, authDispatch] = useReducer(authReducer, initialAuth);
 
-  useEffect(() => {
-    authDispatch({
-      type: "LOGIN_SUCCESS",
-      payload: {
-        userData: JSON.parse(localStorage.getItem("userData")),
-        encodedToken: localStorage.getItem("token") || "",
-      },
-    });
-  }, []);
   return (
     <AuthContext.Provider value={{ authState, authDispatch }}>
       {children}
