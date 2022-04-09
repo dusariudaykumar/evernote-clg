b39250a885fb9248aca22236ee64174e950bb4dd
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-09 19:50:50 +0530

    Update Homepage.jsx

--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -1,12 +1,55 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
 import { NotesCard, NotesField, SideNav } from "../../Components";
+import { useAuth } from "../../contexts";
+import { useNotes } from "../../contexts/notes-context";
 import "./Homepage.css";
+const initialNotes = {
+  notesTitle: "",
+  notesBody: "",
+};
 const Homepage = () => {
+  const {
+    authState: { encodedToken },
+  } = useAuth();
+  const { noteState, noteDispatch } = useNotes();
+  const [notes, setNotes] = useState(initialNotes);
+
+  const addNoteHandler = async (event, note) => {
+    event.preventDefault();
+    console.log(note);
+    try {
+      const response = await axios.post(
+        "/api/notes",
+        { note },
+        {
+          headers: {
+            authorization: encodedToken,
+          },
+        }
+      );
+      noteDispatch({
+        type: "ADD_NOTE",
+        payload: response.data.notes,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  console.log(noteState.notes);
   return (
     <div className="home-page-wrapper">
-      <SideNav />
-      <NotesField />
-      <NotesCard />
+      <NotesField
+        setNotes={setNotes}
+        notes={notes}
+        addNoteHandler={addNoteHandler}
+      />
+      <div className=" notecard-container ">
+        {noteState.notes.map((item) => (
+          <NotesCard key={item._id} notes={item} />
+        ))}
+      </div>
     </div>
   );
 };
