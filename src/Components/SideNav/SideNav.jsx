d7bc9105dd9aac6090c5a8c3a601ebf3f319fde4
Uday Kumar <dudaykumargoud@gmail.com>
2022-04-15 23:26:05 +0530

    added : logout functionality

--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -1,63 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/auth-context";
 import "./SideNav.css";
-const SideNav = ({ setIsnoteInputVisible }) => {
+const SideNav = () => {
   const {
     authState: { userData },
   } = useAuth();
+  const logoutHandler = () => {
+    localStorage.clear();
+    navigate(0);
+  };
+  const navigate = useNavigate();
+
+  const getActiveStyle = () => ({
+    backgroundColor: "#41331c",
+  });
 
   return (
     <div className="side-nav flex-col">
       <ul className="nav-items">
         <li className="nav-pill">
-          <Link to="/home" className="nav-link">
+          <NavLink className="nav-pill" to="/home">
             <div className="flex nav-icon-container">
               <span className="material-icons-outlined side-nav-icon ">
                 home
               </span>
               <span>Home</span>
             </div>
-          </Link>
+          </NavLink>
         </li>
         <li className="nav-pill">
-          <Link to="/home" className="nav-link">
+          <NavLink style={getActiveStyle} to="/home">
             <div className="flex nav-icon-container">
               <span className="material-icons-outlined side-nav-icon">
-                <span className="material-icons-outlined side-nav-icon">
-                  label
-                </span>
+                label
               </span>
               <span>Label</span>
             </div>
-          </Link>
+          </NavLink>
         </li>
         <li className="nav-pill">
-          <Link to="/archive" className="nav-link">
+          <NavLink style={getActiveStyle} to="/archive">
             <div className="flex nav-icon-container">
               <span className="material-icons-outlined side-nav-icon">
                 archive
               </span>
               <span>Archive</span>
             </div>
-          </Link>
+          </NavLink>
         </li>
         <li className="nav-pill">
-          <Link to="/home" className="nav-link">
+          <NavLink style={getActiveStyle} to="/trash">
             <div className="flex nav-icon-container">
               <span className="material-icons-outlined side-nav-icon">
                 delete
               </span>
               <span>Trash</span>
             </div>
-          </Link>
+          </NavLink>
         </li>
       </ul>
       <div className="logout-container flex">
         <div className="user-name">
           {userData.firstName + " " + userData.lastName}
         </div>
-        <span className="material-icons side-nav-icon">logout</span>
+        <span className="material-icons side-nav-icon" onClick={logoutHandler}>
+          logout
+        </span>
       </div>
     </div>
   );
